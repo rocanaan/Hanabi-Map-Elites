a170f576d3c575c6a7b270a12b0f4036880c3cd1
Rodrigo Canaan <rodrigocanaan@Rodrigos-MacBook-Pro.local>
2018-04-03 13:04:20 -0400

    Adding missing file

---
+++ starter/src/main/java/com/fossgalaxy/games/fireworks/ai/EvolutionaryAlgorithm.java
@@ -0,0 +1,49 @@
+package com.fossgalaxy.games.fireworks.ai;
+
+import com.fossgalaxy.games.fireworks.ai.osawa.rules.OsawaDiscard;
+import com.fossgalaxy.games.fireworks.ai.rule.DiscardOldestFirst;
+import com.fossgalaxy.games.fireworks.ai.rule.PlaySafeCard;
+import com.fossgalaxy.games.fireworks.ai.rule.ProductionRuleAgent;
+import com.fossgalaxy.games.fireworks.ai.rule.TellAnyoneAboutUsefulCard;
+import com.fossgalaxy.games.fireworks.ai.rule.random.PlayProbablySafeCard;
+import com.fossgalaxy.games.fireworks.ai.rule.simple.PlayIfCertain;
+
+import java.util.Vector;
+
+import com.fossgalaxy.games.fireworks.GameRunner;
+import com.fossgalaxy.games.fireworks.GameStats;
+import com.fossgalaxy.games.fireworks.players.Player;
+import com.fossgalaxy.games.fireworks.utils.AgentUtils;
+import com.fossgalaxy.stats.BasicStats;
+import com.fossgalaxy.stats.StatsSummary;
+
+public class EvolutionaryAlgorithm {
+	public static void main( String[] args ) {
+		int maxNumPlayers = 5;
+		int numGames = 100;
+		
+		ProductionRuleAgent pra = new ProductionRuleAgent();
+		
+        pra.addRule(new PlayIfCertain());
+        pra.addRule(new PlaySafeCard());
+        pra.addRule(new TellAnyoneAboutUsefulCard());
+        pra.addRule(new OsawaDiscard());
+        pra.addRule(new DiscardOldestFirst());
+
+		AgentPlayer agent = new AgentPlayer("test IGGI", pra);
+		
+	//	Vector<AgentPlayer> otherAgents = new Vector<AgentPlayer>();
+		
+		//AgentPlayer yourAgent = new AgentPlayer(yourAgentName, AgentUtils.buildAgent(yourAgentName));
+//		for (int i = 2; i<=maxNumPlayers; i++) {
+//			otherAgents.add(new AgentPlayer(otherAgentName, AgentUtils.buildAgent(otherAgentName)));
+//		}
+		
+		AgentPlayer otherAgent = new AgentPlayer("other test IGGI", pra);
+		
+		PairingSummary stats = TestSuite.VariableNumberPlayersTest( agent,  otherAgent,  maxNumPlayers,  numGames);
+
+		System.out.println(stats);
+				
+	}
+}
---
+++ starter/src/main/java/com/fossgalaxy/games/fireworks/ai/RunPopulationMirrorEvaluation.java
@@ -0,0 +1,41 @@
+package com.fossgalaxy.games.fireworks.ai;
+
+import java.util.Vector;
+
+import com.fossgalaxy.games.fireworks.GameRunner;
+import com.fossgalaxy.games.fireworks.GameStats;
+import com.fossgalaxy.games.fireworks.players.Player;
+import com.fossgalaxy.games.fireworks.utils.AgentUtils;
+import com.fossgalaxy.stats.BasicStats;
+import com.fossgalaxy.stats.StatsSummary;
+
+/*
+ * This class evaluates an entire population in mirror tests
+ */
+
+public class RunPopulationMirrorEvaluation {
+	public static void main( String[] args ) {
+		int maxNumPlayers = 5;
+		int numGames = 100;
+		
+		String[] agentNames = {"RuleBasedIGGI", "RuleBasedInternal","RuleBasedOuter","SampleLegalRandom","RuleBasedVanDeBergh","RuleBasedFlawed","RuleBasedPiers"};
+		
+		Vector<AgentPlayer> population = new Vector<AgentPlayer>();
+		
+		for(String name: agentNames) {
+			AgentPlayer newAgent = new AgentPlayer(name, AgentUtils.buildAgent(name));
+			population.add(newAgent);
+		}
+		
+		PopulationEvaluationSummary pes = TestSuite.mirrorPopulationEvaluation(population, maxNumPlayers, numGames);
+		
+		System.out.println(pes);
+		
+//		PairingSummary stats = TestSuite.VariableNumberPlayersTest(agentName, otherAgent, maxNumPlayers, numGames );
+//		
+//		int n = 2;
+//	
+//		System.out.println(stats);
+				
+	}
+}
