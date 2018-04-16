107e0d46dc406ac329f5dda89b2f703215693ad0
Rodrigo Canaan <rodrigocanaan@172-16-44-106.DYNAPOOL.NYU.EDU>
2018-04-16 18:35:34 -0400

    added rules to rulebase and methods getBaselineAgentChromossomes and getGoodAgentChromossomes

--- starter/src/main/java/Evolution/Rulebase.java
+++ starter/src/main/java/Evolution/Rulebase.java
@@ -1,6 +1,10 @@
 package Evolution;
 
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.BiFunction;
 
 import com.fossgalaxy.games.fireworks.ai.AgentPlayer;
@@ -116,6 +120,16 @@ public class Rulebase {
 			// Conditional rules
 			new IfRule(hailMary, new  PlayProbablySafeCard(0.0)), //hail mary
 			new IfRule(hailMary, new  PlayProbablySafeCard(0.1)), // slightly smarter hail mary
+			
+			new IfRule(hasMoreThanOneLife, new  PlayProbablySafeCard(0.0)),
+			new IfRule(hasMoreThanOneLife, new  PlayProbablySafeCard(0.2)),
+			new IfRule(hasMoreThanOneLife, new  PlayProbablySafeCard(0.4)),
+			new IfRule(hasMoreThanOneLife, new  PlayProbablySafeCard(0.6)),
+			new IfRule(hasMoreThanOneLife, new  PlayProbablySafeCard(0.8)), // Used by Piers
+			
+			new IfRule(informationLessThan4, new TellDispensable()), //Used by Piers
+			
+			new PlayProbablySafeCard(0.25) // Used by Flawed
 	};
 
 	
@@ -153,6 +167,75 @@ public class Rulebase {
 		
 	}
 	
+	public static int getChromossomeLength ()
+	{
+		return ruleset.length;
+	}
+	
+	public static ArrayList<int[]> getBaselineAgentChromossomes(){
+		ArrayList<int[]> baselinePrefixes = BaselinePrefixPool.getBaselinePrefixes();
+		
+		ArrayList<int[]> baselineChromossomes = new ArrayList<int[]>();
+		
+		for (int[] prefix : baselinePrefixes) {
+			baselineChromossomes.add(randomFillPrefix(prefix));
+		}
+		
+		return baselineChromossomes;
+	}
+	
+	public static ArrayList<int[]> getGoodAgentChromossomes(){
+		ArrayList<int[]> baselinePrefixes = BaselinePrefixPool.getGoodPrefixes();
+		
+		ArrayList<int[]> baselineChromossomes = new ArrayList<int[]>();
+		
+		for (int[] prefix : baselinePrefixes) {
+			baselineChromossomes.add(randomFillPrefix(prefix));
+		}
+		
+		return baselineChromossomes;
+	}
+	
+	private static int[] randomFillPrefix(int[] prefix) {
+		List<Integer> rules = new ArrayList<Integer>();
+		
+		//create a list from 0 to chromossomeLength-1
+		int chromossomeLength = getChromossomeLength();
+		for (int i = 0; i< chromossomeLength; i++) {
+			rules.add(i);
+		}
+		
+		Collections.shuffle(rules);
+		
+		int chromossome[] = new int[chromossomeLength];
+		
+		int currentIndex = 0;
+		
+		for (int j : prefix) {
+			chromossome[currentIndex] = j;
+			currentIndex++;
+		}
+
+		
+		for (Integer i : rules) {
+			boolean duplicate = false;
+			for (int j : prefix) {
+				if (i == j ) {
+					duplicate = true;
+					break;
+				}
+			}
+			if (!duplicate) {
+				chromossome[currentIndex] = i;
+				currentIndex++;
+			}
+		}
+		
+		
+		return chromossome;
+		
+	}
+	
 //	public static void main( String[] args ) {
 //		 ProductionRuleAgent pra = new ProductionRuleAgent();
 //		 
@@ -164,5 +247,78 @@ public class Rulebase {
 //
 //		 System.out.println(stats);
 //	}
+	public static void main(String[] args) {
+		ArrayList<int[]> baseline = getBaselineAgentChromossomes();
+		for (int[] chromossome : baseline) {
+			System.out.printf("[");
+			for (int i : chromossome) {
+				System.out.printf(i+",");
+			}
+			System.out.println("]");
+		}
+		System.out.println("good");
+		ArrayList<int[]> good = getGoodAgentChromossomes();
+		for (int[] chromossome : good) {
+			System.out.printf("[");
+			for (int i : chromossome) {
+				System.out.printf(i+",");
+			}
+			System.out.println("]");
+		}
+	}
+	
+	private static class BaselinePrefixPool{
+		
+		
+		//baselineChromossomes
+		private static int[] cautiousChromossome = {2,8,13,38,34};
+		private static int[] flawedChromossome = {8,49,22,38,27,34};
+		private static int[] iggiChromossome = {2,8,13,38,27};
+		private static int[] internalChromossome = {8,38,20,22,34};
+		private static int[] outerChromossome = {8,38,21,22,34};
+		private static int[] piersChromossome = {41,8,46,13,48,38,27,22,34};
+		private static int[] vandeberghChromossome = {46,37,13,14,19,29}; //Note: actual Van de Bergh uses DiscardProbablyUselessCard(1.0) -not on rulebase- rather than DiscardUselessCard -27
+		
+		//evolved or modified agents
+		private static int[] firstEvolved = {8,13,28,6,33,35,20,38,37,23,30,26,9,0,14,17,10,21,25,4,15,16,11,31,3,5,29,36,7,27,34,12,2,39,19,40,18,24,1,32,22};
+		private static int[] variation1 = {8,13,38,28,6,33,35,20,37,23,30,26,9,0,14,17,10,21,25,4,15,16,11,31,3,5,29,36,7,27,34,12,2,39,19,40,18,24,1,32,22};
+		private static int[] variation2 = {42,8,13,38,28,6,33,35,20,37,23,30,26,9,0,14,17,10,21,25,4,15,16,11,31,3,5,29,36,7,27,34,12,2,39,19,40,18,24,1,32,22};
+		
+
+		public static ArrayList<int[]> getBaselinePrefixes(){
+			ArrayList<int[]> baselineChromossomes = new ArrayList<int[]>();
+			
+			baselineChromossomes.add(cautiousChromossome);
+			baselineChromossomes.add(flawedChromossome);
+			baselineChromossomes.add(iggiChromossome);
+			baselineChromossomes.add(internalChromossome);
+			baselineChromossomes.add(outerChromossome);
+			baselineChromossomes.add(piersChromossome);
+			baselineChromossomes.add(vandeberghChromossome);
+			
+			return baselineChromossomes;
+		}
+		
+		public static ArrayList<int[]> getGoodPrefixes(){
+			ArrayList<int[]> baselineChromossomes = new ArrayList<int[]>();
+			
+			baselineChromossomes.add(cautiousChromossome);
+			baselineChromossomes.add(iggiChromossome);
+			baselineChromossomes.add(internalChromossome);
+			baselineChromossomes.add(outerChromossome);
+			baselineChromossomes.add(piersChromossome);
+			baselineChromossomes.add(vandeberghChromossome);
+			baselineChromossomes.add(firstEvolved);
+			baselineChromossomes.add(variation1);
+			baselineChromossomes.add(variation2);
+			
+			return baselineChromossomes;
+		}
+
+
+	}
+	
+	
+	
 
 }
